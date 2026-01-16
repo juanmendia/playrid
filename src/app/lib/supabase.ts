e6d6818354487d6fc12c34f45f1fa120483a3302
Mendia Juan Esteban <Juan.Mendia@bancodelapampa.com.ar>
2026-01-16 09:44:43 -0300

    update supbase.st

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "./database.types";
 
-const supabaseUrl = "https://wncezclaxdnoyeqoggmn.supabase.co";
-const supabaseAnonKey = "sb_publishable_AFHcogrjLh86t93Utv5j8w_u_UuUG-k";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export function createClient() {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
